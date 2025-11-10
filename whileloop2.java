2e1663903a7e938091b9411cbbfbb7ceac034503
Ishaan Sharma <128037158+i-shaansharma@users.noreply.github.com>
2025-11-10 19:33:15 +0530

    Add files via upload

---
+++ whileloop2.java
@@ -0,0 +1,10 @@
+public class whileloop2 {
+    public static void main(String[] args) {
+        int counter = 0; // loop starts
+        while ( counter <= 69) {
+            System.out.println("Hello world");
+            counter++;
+        }
+        System.out.println("Loop ended");
+    }
+}
