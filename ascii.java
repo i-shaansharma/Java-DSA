daff7cbe8a2e6dfd0e0c702874e8b522a2fe938c
Ishaan Sharma <128037158+i-shaansharma@users.noreply.github.com>
2025-11-12 23:41:17 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,6 @@
+public class ascii {
+    public static void main(String[] args) {
+        int[] code = {72,101,108,108,111,44,32,87,111,114,108,100,33};
+        for (int c : code) System.out.print((char)c);
+    }
+}
